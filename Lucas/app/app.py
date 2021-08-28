3133fca853952ab3351e90f51bb2d73c6c697f87
Lucas Munson <lucasbmunson@gmail.com>
2021-08-28 11:58:07 -0400

    more working api with index by ticker symbol

--- Lucas/app/app.py
+++ Lucas/app/app.py
@@ -1,5 +1,23 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, json, render_template, redirect, request
 from flask import jsonify
+
+# ----- dummy data
+data={
+
+    "A" : {
+        "ticker": "A",
+        "closing" : "3.44",
+        "open" : "3",
+        "sentiment" : "0.323"
+    },
+
+    "B" : {
+        "ticker": "B",
+        "closing" : "10",
+        "open" : "1444",
+        "sentiment" : "-0.9"
+    }
+}
 
 #----------------- BASIC ENDPOINTS -------------------- 
 
@@ -10,11 +28,16 @@ def welcome():
     return render_template("index.html")
 
 # API works by passing quereys e.g. {url}/query?key=value&key=value&key=value
-#each key value pair is seperated by & and the full query is preceeded by a ? 
+# each key value pair is seperated by & and the full query is preceeded by a ? 
+# Structure: we index the data by ticker symbol and then make an API call for that ticker symbol. Should return json object with all data for that symbol. 
+# need another endpoint to do searches by date
 @app.route("/query", methods=['GET', 'POST'])
 def query():
-    thing = request.args.get('thing')
-    return thing
+    input = request.args.get('ticker')
+    print(input)
+    #here use this key to find the corrisponding value and return it. Should pass a stock ticker and return a json object with all the correponding data. 
+    
+    return jsonify(data[input])
 
 
 #---------------- API BELOW -----------------
