8d0cdc5ba300c381a152fce72f69cb464ca04dc2
Lucas Munson <lucasbmunson@gmail.com>
2021-09-09 20:36:32 -0400

    flask routing fixes

--- app/app.py
+++ app/app.py
@@ -34,18 +34,10 @@ def heatmap_redirect():
 # need another endpoint to do searches by date
 @app.route("/query", methods=['GET', 'POST'])
 def query():
-    input = request.args.get('ticker')
+    
     #here use this key to find the corrisponding value and return it. Should pass a stock ticker and return a json object with all the correponding data. 
     
     return (data)
-
-
-#---------------- API BELOW -----------------
-
-@app.route('/person/')
-def hello():
-    return jsonify({'name':'Jimit',
-                    'address':'India'})
 
 
 #------------ RUN FLASK --------------------
